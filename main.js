df332328d6a8b10cfad420a67e62a883643fc4df
Luke <luke.goddard44@gmail.com>
2022-03-20 16:37:17 +1300

    Add board drawing function

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-console.log('works');
+// Initialize an array to store game state
+const state = ['', '', '', '', '', '', '', '', '']
 
-// 
+// Initialize start button
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', drawBoard);
+
+// Assign board to a variable
+const board = document.getElementById('board');
+
+// Draw initial map from game state array
+function drawBoard(arr) {
+  board.innerHTML = '';
+  // Recursively draw squares for each item in the array
+  state.forEach(item => {
+    let elem = document.createElement('div');
+    elem.classList.add('square');
+    elem.innerHTML = 'HI';
+    board.appendChild(elem);
+  })
+  
+  // Add event listeners to each square
+}
+
