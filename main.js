22e2074636b5cd33770522291952ebd4b841677e
Luke <luke.goddard44@gmail.com>
2022-03-20 17:08:02 +1300

    Make move funtion trigger when square is clicked

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 // Initialize an array to store game state
 const state = ['', '', '', '', '', '', '', '', '']
 
-// Initialize start button
-const startButton = document.getElementById('start');
-startButton.addEventListener('click', drawBoard);
+// Var to which turn it is
+let turn = 'X'
+
+// Func for changing turns
+function changeTurn() {
+  turn = turn == 'X' ? 'O' : 'X';
+  document.getElementById('currentTurn').innerHTML = turn;
+}
+
+changeTurn();
 
 // Assign board to a variable
 const board = document.getElementById('board');
+
+// Initialize start button
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', () => drawBoard(state));
+
+
+// function for when a move is made
+function move(elem) {
+  alert(elem.id);
+}
 
 // Draw initial map from game state array
 function drawBoard(arr) {
   board.innerHTML = '';
   // Recursively draw squares for each item in the array
-  state.forEach(item => {
+  for (let i = 0; i < arr.length; i++) {
     let elem = document.createElement('div');
     elem.classList.add('square');
-    elem.innerHTML = 'HI';
+    elem.setAttribute('id', i)
+    elem.innerHTML = 'X';
+    elem.addEventListener('click', () => move(elem));
     board.appendChild(elem);
-  })
+  }
   
   // Add event listeners to each square
 }
